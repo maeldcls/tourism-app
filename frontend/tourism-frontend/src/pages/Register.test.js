c170eb5dd3f5e63b02743d583553335f272dbf19
maeldcls <maelduclos03@gmail.com>
2026-06-12 09:05:28 +0200

    fixing issues and testing pipeline

--- frontend/tourism-frontend/src/pages/Register.test.js
+++ frontend/tourism-frontend/src/pages/Register.test.js
@@ -2,6 +2,8 @@ import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import axios from 'axios';
 
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 import Register from './Register';
 import { AuthProvider } from '../context/AuthContext';
 
@@ -15,9 +17,11 @@ jest.mock('react-router-dom', () => ({
 
 function renderRegister() {
   return render(
-    <AuthProvider>
-      <Register />
-    </AuthProvider>
+    <GoogleOAuthProvider clientId="test-client-id">
+      <AuthProvider>
+        <Register />
+      </AuthProvider>
+    </GoogleOAuthProvider>
   );
 }
 
