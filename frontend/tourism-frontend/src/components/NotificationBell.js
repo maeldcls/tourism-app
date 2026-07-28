2b59b9580e0b48eea7ce49f45a9c81e3c1ad4b38
maeldcls <maelduclos03@gmail.com>
2026-07-28 10:33:49 +0200

    manage profil, added friends system and can add people to trips fixed

--- frontend/tourism-frontend/src/components/NotificationBell.js
+++ frontend/tourism-frontend/src/components/NotificationBell.js
@@ -15,6 +15,7 @@ export default function NotificationBell() {
 
   useEffect(() => {
     fetchNotifications();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [token]);
 
   useEffect(() => {
